f8522581794e96ef7becec83115b3e7cf6f5d544
Duy <doubled@yahoo.com>
2015-03-03 21:27:58 -0800

    File stream exercise example

---
+++ src/fileObject/CopyFile.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileObject;
+
+import java.io.*;
+
+/**
+ *
+ * @author User
+ */
+public class CopyFile {
+    public static void main(String[] args){
+        try{
+            FileInputStream in = new FileInputStream("image.jpg");            
+            BufferedInputStream bin = new BufferedInputStream(in);
+            
+            FileOutputStream out = new FileOutputStream("target.jpg");
+            BufferedOutputStream bout = new BufferedOutputStream(out);     
+            
+            byte[] info = new byte[100];
+            while (bin.read(info) != -1){
+                bout.write(info);
+            }
+            
+            bin.close();
+            bout.close();            
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+            return;
+        }
+    }
+}
--- src/fileObject/FileObjectExercise.java
+++ src/fileObject/FileObjectExercise.java
@@ -12,21 +12,21 @@ import java.io.File;
  * @author User
  */
 public class FileObjectExercise {
-    public static void main(String[] args){
-        File myDir = new File("C:/javatemp");
-        System.out.println(myDir.isDirectory() ? "yep" : "nope");
-        
-        File myFile = new File(myDir, "file.txt");
-        System.out.println(myFile.getName());
-        System.out.println(myFile.getPath());
-        
-        File newDir = new File(myDir, "newDir");
-        newDir.mkdir();
-        String[] folders = myDir.list();
-        
-       for(String f :folders){
-           System.out.println(f);
-       }
-
-    }    
+//    public static void main(String[] args){
+//        File myDir = new File("C:/javatemp");
+//        System.out.println(myDir.isDirectory() ? "yep" : "nope");
+//        
+//        File myFile = new File(myDir, "file.txt");
+//        System.out.println(myFile.getName());
+//        System.out.println(myFile.getPath());
+//        
+//        File newDir = new File(myDir, "newDir");
+//        newDir.mkdir();
+//        String[] folders = myDir.list();
+//        
+//       for(String f :folders){
+//           System.out.println(f);
+//       }
+//
+//    }    
 }
